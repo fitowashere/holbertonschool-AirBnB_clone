2b66c052f51bb63b18eac1af214e6c2a0c54e0f5
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-10-30 16:12:14 -0400

    fix spacing document

--- models/base_model.py
+++ models/base_model.py
@@ -1,29 +1,26 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
-
 import uuid
 from datetime import datetime
 import models
+
 
 class BaseModel:
     """A base class for all hbnb models"""
 
     def __init__(self):
-        """ a new model"""
+        """Initialize a new BaseModel."""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        models.storage.new(self)
-        models.storage.save()
-    
+
     def __str__(self):
         """Returns a string representation of the instance"""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-    
+        return F"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
     def save(self):
         """Updates updated_at with the current datetime"""
         self.updated_at = datetime.now()
-        models.storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__ of the instance"""
@@ -32,4 +29,3 @@ class BaseModel:
         new_dict["created_at"] = self.created_at.isoformat()
         new_dict["updated_at"] = self.updated_at.isoformat()
         return new_dict
-    
