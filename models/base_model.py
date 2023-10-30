c6042344bebf18f93e63021f0c9fdf0063f32ceb
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-10-30 17:30:40 -0400

    fixing structure

--- models/base_model.py
+++ models/base_model.py
@@ -8,20 +8,12 @@ import models
 class BaseModel:
     """BaseModel class"""
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         """Constructor"""
-        if kwargs:
-            for k, v in kwargs.items():
-                if k in ['created_at', 'updated_at']:
-                    t_format = '%Y-%m-%dT%H:%M:%S.%f'
-                    self.__dict__[k] = datetime.strptime(v, t_format)
-                elif k != '__class__':
-                    self.__dict__[k] = v
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        models.storage.new(self)
 
     def __str__(self):
         """str method"""
@@ -39,3 +31,16 @@ class BaseModel:
         new_dict["created_at"] = self.created_at.isoformat()
         new_dict["updated_at"] = self.updated_at.isoformat()
         return new_dict
+
+
+my_model = BaseModel()
+my_model.name = "My First Model"
+my_model.my_number = 89
+print(my_model)
+my_model.save()
+print(my_model)
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
