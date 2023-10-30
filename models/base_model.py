4e08bd969f9ee7c58b836527f73a40af02fdf056
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-10-30 16:16:36 -0400

    save storage fix

--- models/base_model.py
+++ models/base_model.py
@@ -31,6 +31,7 @@ class BaseModel:
     def save(self):
         """Updates updated_at with the current datetime"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__ of the instance"""
