7ea0d486cd1ab9a8bb9e5647c744ab93acd6e794
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-10-30 15:57:12 -0400

    str save to dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -8,13 +8,28 @@ import models
 class BaseModel:
     """A base class for all hbnb models"""
 
-    
     def __init__(self):
-        """Instatntiates a new model"""
+        """ a new model"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
         models.storage.new(self)
         models.storage.save()
     
+    def __str__(self):
+        """Returns a string representation of the instance"""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+    
+    def save(self):
+        """Updates updated_at with the current datetime"""
+        self.updated_at = datetime.now()
+        models.storage.save()
 
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__ of the instance"""
+        new_dict = self.__dict__.copy()
+        new_dict["__class__"] = self.__class__.__name__
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        return new_dict
+    
