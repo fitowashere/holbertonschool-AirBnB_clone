8224a994bd75c44b75089b1529626a7dff742e9e
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-10-30 16:51:05 -0400

    base model test

--- models/base_model.py
+++ models/base_model.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-"""This module defines a base class for all models in our hbnb clone"""
+"""Module base_model"""
 import uuid
 from datetime import datetime
 import models
 
 
 class BaseModel:
-    """A base class for all hbnb models"""
+    """BaseModel class"""
 
     def __init__(self, *args, **kwargs):
-        """Initialize a new BaseModel."""
+        """Constructor"""
         if kwargs:
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    if key == "created_at" or key == "updated_at":
-                        setattr(self, key, datetime.strptime(value,
-                                                             "%Y-%m-%dT%H:%M:%S.%f"))
-                    else:
-                        setattr(self, key, value)
+            for k, v in kwargs.items():
+                if k in ['created_at', 'updated_at']:
+                    t_format = '%Y-%m-%dT%H:%M:%S.%f'
+                    self.__dict__[k] = datetime.strptime(v, t_format)
+                elif k != '__class__':
+                    self.__dict__[k] = v
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -25,16 +24,16 @@ class BaseModel:
             models.storage.new(self)
 
     def __str__(self):
-        """Returns a string representation of the instance"""
-        return F"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        """str method"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """Updates updated_at with the current datetime"""
+        """save method"""
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__ of the instance"""
+        """returns a dictionary containing all keys"""
         new_dict = self.__dict__.copy()
         new_dict["__class__"] = self.__class__.__name__
         new_dict["created_at"] = self.created_at.isoformat()
