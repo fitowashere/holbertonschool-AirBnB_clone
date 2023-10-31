998090c8fb2ac529de0bcbda174f0fced654df3e
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-10-31 14:37:04 -0400

    user

--- models/user.py
+++ models/user.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """User class that inherits from BaseModel"""
 from models.base_model import BaseModel
+import models
+from datetime import datetime
 
 class User(BaseModel):
     """User class that inherits from BaseModel"""
@@ -8,3 +10,25 @@ class User(BaseModel):
     password = ""
     first_name = ""
     last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Constructor\n"""
+        super().__init__(*args, **kwargs)
+        models.storage.new(self)
+
+    def __str__(self):
+        """str method\n"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """save method\n"""
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """to_dict method\n"""
+        dic_copy = self.__dict__.copy()
+        dic_copy['__class__'] = self.__class__.__name__
+        dic_copy['created_at'] = self.created_at.isoformat()
+        dic_copy['updated_at'] = self.updated_at.isoformat()
+        return dic_copy
