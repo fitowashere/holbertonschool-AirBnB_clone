6acd5b7d0d9e656611bf4265894937bafa8b108b
ControlYourPotatoes <alexanderpuga78@gmail.com>
2023-10-31 13:06:45 -0400

    BASE

---
+++ base_modelbak.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Base Model"""
+from . import storage
+from datetime import datetime
+import uuid
+
+
+class BaseModel():
+    """Defines all common attributes/methods"""
+    def __init__(self, *args, **kwargs):
+        """BaseModel class constructor"""
+        if kwargs is None or len(kwargs) == 0:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
+        else:
+            ISO_fmt = '%Y-%m-%dT%H:%M:%S.%f'
+            self.created_at = datetime.strptime(kwargs['created_at'], ISO_fmt)
+            self.updated_at = datetime.strptime(kwargs['updated_at'], ISO_fmt)
+            for key, value in kwargs.items():
+                if key not in ('created_at', 'updated_at', '__class__'):
+                    self.__dict__[key] = value
+
+    def __str__(self):
+        """Returns the string representation of BaseModel"""
+        return "[{}] ({}) {}".format(
+                self.__class__.__name__, self.id, str(self.__dict__))
+
+    def save(self):
+        """Updates updated_at with the current datetime"""
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values"""
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = self.__class__.__name__
+        my_dict["created_at"] = self.created_at.isoformat()
+        my_dict["updated_at"] = self.updated_at.isoformat()
+        return my_dict
--- models/base_model.py
+++ models/base_model.py
@@ -1,41 +1,37 @@
-#!/usr/bin/python3
-"""Base Model"""
-from . import storage
-from datetime import datetime
 import uuid
+from datetime import datetime
 
-
-class BaseModel():
-    """Defines all common attributes/methods"""
-    def __init__(self, *args, **kwargs):
-        """BaseModel class constructor"""
-        if kwargs is None or len(kwargs) == 0:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
-        else:
-            ISO_fmt = '%Y-%m-%dT%H:%M:%S.%f'
-            self.created_at = datetime.strptime(kwargs['created_at'], ISO_fmt)
-            self.updated_at = datetime.strptime(kwargs['updated_at'], ISO_fmt)
-            for key, value in kwargs.items():
-                if key not in ('created_at', 'updated_at', '__class__'):
-                    self.__dict__[key] = value
+class BaseModel:
+    """
+    BaseModel class that defines all common attributes/methods for other classes
+    """
+    def __init__(self):
+        """
+        Constructor method that initializes the BaseModel instance
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        
 
     def __str__(self):
-        """Returns the string representation of BaseModel"""
-        return "[{}] ({}) {}".format(
-                self.__class__.__name__, self.id, str(self.__dict__))
+        """
+        Method that returns a string representation of the BaseModel instance
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """Updates updated_at with the current datetime"""
+        """
+        Method that updates the public instance attribute updated_at with the current datetime
+        """
         self.updated_at = datetime.now()
-        storage.save()
 
     def to_dict(self):
-        """Returns a dictionary containing all keys/values"""
-        my_dict = self.__dict__.copy()
-        my_dict["__class__"] = self.__class__.__name__
-        my_dict["created_at"] = self.created_at.isoformat()
-        my_dict["updated_at"] = self.updated_at.isoformat()
-        return my_dict
+        """
+        Method that returns a dictionary containing all keys/values of __dict__ of the instance
+        """
+        new_dict = self.__dict__.copy()
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        return new_dict
